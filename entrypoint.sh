1925aab7ac1adfce10e4dfe3679a78dfb8859248
John Strunk <jstrunk@redhat.com>
2025-08-29 13:52:52 -0400

    Update Dockerfile and entrypoint scripts
    Refactor Dockerfile to use environment variables for Python tools and
    update entrypoint scripts to improve user management and script execution.
    
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 set -e -o pipefail
 
-if [[ -z "$EUID" || -z "$EGID" ]]; then
-    exec "$@"
-fi
-
+EUID="${EUID:-1000}"
+EGID="${EGID:-1000}"
 USERNAME="user"
+GROUPNAME="user"
 
 if [[ $# -lt 1 ]]; then
     # Set default command to bash if no arguments are provided
     set -- bash
 fi
 
-groupadd -g "$EGID" "$USERNAME" || true
-useradd -u "$EUID" -g "$EGID" -m "$USERNAME"
+HOMEDIR="/home/$USERNAME"
+groupadd -g "$EGID" "$GROUPNAME" || true
+useradd -o -u "$EUID" -g "$EGID" -m "$USERNAME"
 
 # Give the user access to the Docker socket if it exists
 if [[ -S /var/run/docker.sock ]]; then
@@ -21,17 +21,15 @@ if [[ -S /var/run/docker.sock ]]; then
     groupadd -g "$DOCKER_GID" docker || true
     usermod -aG docker "$USERNAME"
 fi
-chown "$USERNAME":"$USERNAME" "/.pre-commit"
+chown "$USERNAME":"$GROUPNAME" "/.pre-commit"
 
-gosu "$USERNAME" mkdir -p "/home/$USERNAME/.config"
-gosu "$USERNAME" mkdir -p "/home/$USERNAME/.cache"
+gosu "$USERNAME" mkdir -p "$HOMEDIR/.config"
+gosu "$USERNAME" mkdir -p "$HOMEDIR/.cache"
 
-ln -s /.gemini "/home/$USERNAME/.gemini"
-ln -s /.claude "/home/$USERNAME/.claude"
-ln -s /.claude.json "/home/$USERNAME/.claude.json"
-ln -s /.gcloud "/home/$USERNAME/.config/gcloud"
-ln -s /.pre-commit "/home/$USERNAME/.cache/pre-commit"
-gosu "$USERNAME" pre-commit gc > /dev/null 2>&1 || true
-(gosu "$USERNAME" pre-commit install-hooks > /dev/null 2>&1 || true) &
+ln -s /.gemini "$HOMEDIR/.gemini"
+ln -s /.claude "$HOMEDIR/.claude"
+ln -s /.claude.json "$HOMEDIR/.claude.json"
+ln -s /.gcloud "$HOMEDIR/.config/gcloud"
+ln -s /.pre-commit "$HOMEDIR/.cache/pre-commit"
 
-exec gosu "$USERNAME" "$@"
+exec gosu "$USERNAME" /entrypoint_user.sh "$@"
