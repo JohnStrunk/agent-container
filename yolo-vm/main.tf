602fb1e95ee0767128fe01cde4e497817e4abed5
John Strunk <jstrunk@redhat.com>
2025-10-30 13:44:13 -0400

    feat: add main Terraform configuration for VM

---
+++ yolo-vm/main.tf
@@ -0,0 +1,91 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.8.0"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+# Read all SSH public keys from directory
+locals {
+  ssh_key_files = fileset(var.ssh_keys_dir, "*.pub")
+  ssh_keys = [
+    for f in local.ssh_key_files :
+    trimspace(file("${var.ssh_keys_dir}/${f}"))
+  ]
+}
+
+# Download Debian cloud image
+resource "libvirt_volume" "debian_base" {
+  name   = "debian-13-base.qcow2"
+  pool   = "default"
+  source = var.debian_image_url
+  format = "qcow2"
+}
+
+# Create VM disk from base image
+resource "libvirt_volume" "debian_disk" {
+  name           = "${var.vm_name}-disk.qcow2"
+  pool           = "default"
+  base_volume_id = libvirt_volume.debian_base.id
+  size           = var.vm_disk_size
+  format         = "qcow2"
+}
+
+# Cloud-init configuration
+data "template_file" "cloud_init" {
+  template = file("${path.module}/cloud-init.yaml.tftpl")
+  vars = {
+    hostname     = var.vm_hostname
+    default_user = var.default_user
+    ssh_keys     = jsonencode(local.ssh_keys)
+  }
+}
+
+resource "libvirt_cloudinit_disk" "cloud_init" {
+  name      = "${var.vm_name}-cloud-init.iso"
+  pool      = "default"
+  user_data = data.template_file.cloud_init.rendered
+}
+
+# Define the VM
+resource "libvirt_domain" "debian_vm" {
+  name   = var.vm_name
+  memory = var.vm_memory
+  vcpu   = var.vm_vcpu
+
+  cloudinit = libvirt_cloudinit_disk.cloud_init.id
+
+  disk {
+    volume_id = libvirt_volume.debian_disk.id
+  }
+
+  network_interface {
+    network_name   = "default"
+    wait_for_lease = true
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "1"
+    target_type = "virtio"
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
